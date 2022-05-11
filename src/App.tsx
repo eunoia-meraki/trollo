648d1ebb847f81914740ab87dc8a365b81ed5b81
ivan-kuricyn <kuritsin020994@gmail.com>
2022-05-12 00:22:26 +0300

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,25 @@ import { Error404 } from './views/Error404';
 import './App.css';
 import axios from 'axios';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Footer } from './components/Footer';
 
 axios.defaults.baseURL = 'https://evening-bastion-08665.herokuapp.com/';
 
 export const App: FC = () => {
   return (
-    <QueryClientProvider client={new QueryClient()}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/welcome" element={<Welcome />} />
-          <Route path="error-404" element={<Error404 />} />
-          <Route path="*" element={<Navigate to="error-404" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <div className="h-screen flex flex-col">
+      <QueryClientProvider client={new QueryClient()}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/welcome" element={<Welcome />} />
+            <Route path="error-404" element={<Error404 />} />
+            <Route path="*" element={<Navigate to="error-404" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+
+      <Footer />
+    </div>
   );
 };
 
